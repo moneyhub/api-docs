b17c6e0d6a719287617739ac50a3565f365fd2c9
Stefan Spita <1955784+stefanspita@users.noreply.github.com>
2019-02-19 11:19:03 +0000

    Update deployment script to pick up the docs from prod services
    MH3-1878

--- deploy.sh
+++ deploy.sh
@@ -28,9 +28,9 @@ run_build() {
 update_docs() {
   # get the doc files from identity service and api gateway and merge them
   # together into index.html.md
-  curl -o ./source/identity-docs.md  http://identity.dev.127.0.0.1.nip.io/docs.md
-  curl -o ./source/api-gateway-docs.md  http://apigateway.dev.127.0.0.1.nip.io/docs.md
-  cat ./source/identity-docs.md ./source/api-gateway-docs.md > ./source/index.html.md
+  curl -o ./source/identity-docs.md  https://identity.moneyhub.co.uk/auth-docs.md
+  curl -o ./source/api-gateway-docs.md  https://api.moneyhub.co.uk/docs.md
+  cat ./source/headers.md ./source/identity-docs.md ./source/api-gateway-docs.md > ./source/index.html.md
 
   git add .
   git commit -m "Update docs"
---
+++ source/headers.md
@@ -0,0 +1,18 @@
+---
+title: Moneyhub API Documentation
+language_tabs:
+  - shell: Shell
+  - http: HTTP
+  - javascript: JavaScript
+  - javascript--nodejs: Node.JS
+  - ruby: Ruby
+  - python: Python
+  - java: Java
+  - go: Go
+toc_footers: []
+includes: []
+search: true
+highlight_theme: darkula
+headingLevel: 2
+
+---
