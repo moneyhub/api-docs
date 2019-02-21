ab5a1d9791beb1eb37c622cbb13481df1c3d63c4
Stefan Spita <1955784+stefanspita@users.noreply.github.com>
2019-02-21 09:44:54 +0000

    Add separate script for pulling in the docs to test locally
    MH3-1878

---
+++ get-latest-docs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -o ./source/identity-docs.md  https://identity.moneyhub.co.uk/auth-docs.md
+curl -o ./source/api-gateway-docs.md  https://api.moneyhub.co.uk/docs.md
+cat ./source/headers.md ./source/identity-docs.md ./source/api-gateway-docs.md > ./source/index.html.md
