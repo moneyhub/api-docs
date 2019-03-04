bb0e0c39af8f8b799472cffeb058efa94e7e6e62
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-03-04 10:29:05 +0000

    Update get latest docs script

--- get-latest-docs.sh
+++ get-latest-docs.sh
@@ -2,4 +2,6 @@
 
 curl -o ./source/identity-docs.md  https://identity.moneyhub.co.uk/auth-docs.md
 curl -o ./source/api-gateway-docs.md  https://api.moneyhub.co.uk/docs.md
+# curl -o ./source/identity-docs.md  http://identity.dev.127.0.0.1.nip.io/auth-docs.md
+# curl -o ./source/api-gateway-docs.md  http://apigateway.dev.127.0.0.1.nip.io/docs.md
 cat ./source/headers.md ./source/identity-docs.md ./source/api-gateway-docs.md > ./source/index.html.md
