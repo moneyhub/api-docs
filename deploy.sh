f82cb334dfc4ca0352c93c0c65bcb9c85bffbd4c
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-05-29 08:25:36 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -28,10 +28,11 @@ run_build() {
 update_docs() {
   # get the doc files from identity service and api gateway and merge them
   # together into index.html.md
-  curl -o ./source/identity-docs.md  https://identity.moneyhub.co.uk/auth-docs.md
-  curl -o ./source/api-gateway-docs.md  https://api.moneyhub.co.uk/docs.md
 
+  curl -o ./source/identity-docs.md  https://identity.moneyhub.co.uk/auth-docs.md
   # curl -o ./source/identity-docs.md  http://identity.dev.127.0.0.1.nip.io/auth-docs.md
+
+  curl -o ./source/api-gateway-docs.md  https://api.moneyhub.co.uk/docs.md
   # curl -o ./source/api-gateway-docs.md  http://apigateway.dev.127.0.0.1.nip.io/docs.md
 
   cat ./source/headers.md ./source/identity-docs.md ./source/api-gateway-docs.md > ./source/index.html.md
